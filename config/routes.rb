41edd72eab8a4b1bbd956fad46fbeb671f9e222a
BjornMelgaard <melgaardbjorn@gmail.com>
2017-01-24 10:42:23 +0200

    devise migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  devise_for :users
+  devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

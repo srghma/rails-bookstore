eef438bfe1ba66892cd89bba4f8ceeb272337e84
BjornMelgaard <melgaardbjorn@gmail.com>
2017-01-24 21:35:32 +0200

    stage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index
+    # @best_sellers = Book.best_sellers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  resources :home, only: %i(index)
+
   devise_for :users
-  devise_for :admins
+  # devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end

77120797f21dfcd901399e08e2bef64d7cb7be7d
BjornMelgaard <melgaardbjorn@gmail.com>
2017-01-26 17:54:11 +0200

    stage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :home, only: %i(index)
-
   devise_for :users
-  # devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end

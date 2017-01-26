b6017bc05ec2a3991aa15edd6daf04c124d8f3f7
BjornMelgaard <melgaardbjorn@gmail.com>
2017-01-26 20:02:57 +0200

    join with index

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20170126172304) do
   create_table "authors_books", id: false, force: :cascade do |t|
     t.integer "book_id",   null: false
     t.integer "author_id", null: false
+    t.index ["author_id", "book_id"], name: "index_authors_books_on_author_id_and_book_id"
+    t.index ["book_id", "author_id"], name: "index_authors_books_on_book_id_and_author_id"
   end
 
   create_table "books", force: :cascade do |t|

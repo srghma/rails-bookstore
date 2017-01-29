6aec3d6bb273804e89702e3d4087eff5402f1a44
BjornMelgaard <melgaardbjorn@gmail.com>
2017-01-29 18:19:15 +0200

    current_order setter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_current_order
+
+  attr_reader :current_order
+
+  private
+
+  def set_current_order
+    @current_order = Order.find(session[:order_id])
+  rescue ActiveRecord::RecordNotFound
+    create_current_order
+  end
+
+  def create_current_order
+    @current_order = Order.create
+    session[:order_id] = @current_order.id
+  end
 end

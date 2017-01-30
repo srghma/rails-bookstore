bb7314b2ae58dd8086e43584a01a21b6ff186d1e
BjornMelgaard <melgaardbjorn@gmail.com>
2017-01-30 12:57:24 +0200

    current order lookup issue resolve

--- lib/current_order.rb
+++ lib/current_order.rb
@@ -5,23 +5,20 @@ module CurrentOrder
     before_action :set_current_order
     helper_method :current_order
 
-    def current_order
-      @current_order
-    end
-
+    attr_reader :current_order
 
     def set_current_order
-      # puts 'set_current_order'
-      @current_order = Order.find(session[:order_id])
+      # XXX very important to address Order like this, otherwise
+      # rails will not find second time, more here
+      # http://urbanautomaton.com/blog/2013/08/27/rails-autoloading-hell/
+      @current_order = ::Order.find(session[:order_id])
     rescue ActiveRecord::RecordNotFound
       create_current_order
     end
 
     def create_current_order
-      puts 'create_current_order'
-      @current_order = Order.create
+      @current_order = ::Order.create
       session[:order_id] = @current_order.id
     end
   end
-
 end

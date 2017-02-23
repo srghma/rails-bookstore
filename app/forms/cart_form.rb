f2a7fa7bf6668980b4c29f61706d0342ac0f9a66
BjornMelgaard <melgaardbjorn@gmail.com>
2017-02-23 21:40:52 +0200

    working

--- app/forms/cart_form.rb
+++ app/forms/cart_form.rb
@@ -1,10 +1,4 @@
 class CartForm < Rectify::Form
-  attribute :coupon_code, String
+  attribute :coupon, CouponForm
   attribute :products, Array[ProductForm]
-
-  validate :check_coupon_code
-
-  def check_coupon_code
-    errors.add(:coupon_code, 'is invalid') unless Coupon.find_by(code: coupon_code)
-  end
 end
---
+++ app/forms/coupon_form.rb
@@ -0,0 +1,9 @@
+class CouponForm < Rectify::Form
+  attribute :code, String
+
+  validate :check_code
+
+  def check_code
+    errors.add(:code, 'is invalid') unless Coupon.find_by(code: coupon_code)
+  end
+end

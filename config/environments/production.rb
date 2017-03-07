9ddb72eccbdc473b565246c6a82c6369774eebbb
BjornMelgaard <melgaardbjorn@gmail.com>
2017-03-07 23:49:43 +0200

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.app_domain = ENV['DOMAIN_NAME']
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: config.app_domain,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
 end

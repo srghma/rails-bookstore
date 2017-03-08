6261aade194d203f678405729c83e35621abad5b
BjornMelgaard <melgaardbjorn@gmail.com>
2017-03-08 18:28:08 +0200

    email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['DOMAIN_NAME']
+  default from: ENV['MAIL_SENDER']
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.app_domain = ENV['DOMAIN_NAME']
+  config.app_domain = 'tranquil-plains-56319.herokuapp.com'
   config.action_mailer.default_url_options = { host: config.app_domain }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true

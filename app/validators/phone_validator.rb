4324759ba79bccc34b6e87b3c62df16befb3bab8
BjornMelgaard <melgaardbjorn@gmail.com>
2017-04-07 11:43:57 +0300

    imdone

--- app/validators/phone_validator.rb
+++
@@ -1,9 +0,0 @@
-class PhoneValidator < ActiveModel::EachValidator
-  PHONE_REGEX = /\+\d{3}\ \d{2}\ \d{3}\ \d{4}/
-
-  def validate_each(record, attribute, value)
-    return if PHONE_REGEX.match? value
-    message = options[:message] || :phone
-    record.errors.add attribute, message
-  end
-end
